e80497f5672c73d8f5262784e965a76c63cb8432
SantiagoRodriguezP <santiago.rodriguez@sinco.com.co>
2023-04-13 21:44:42 -0500

    Proyecto base

---
+++ src/main/java/com/usergio/admincar/AdmincarApplication.java
@@ -0,0 +1,13 @@
+package com.usergio.admincar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmincarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmincarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usergio/admincar/AdmincarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usergio.admincar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmincarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
